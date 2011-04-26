d3a111029439dd83a97e08c2909da4d8ff4c1b88
David Wilhelm <dewilhelm@gmail.com>
2011-04-26 00:21:42 -0400

    added first test (incomplete)

---
+++ tests/LineTest.js
@@ -0,0 +1,9 @@
+/*global TestCase geometry fail assertTrue*/
+/*jslint newcap:false*/
+
+TestCase("LineTest", {
+    testConstructor: function () {
+        //var line = new geometry.Line();
+        fail("this should fail");
+    }
+});

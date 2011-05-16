d15b2d54b7070ec0d592ac694f23a2e546ec397e
David Wilhelm <dewilhelm@gmail.com>
2011-05-15 21:03:38 -0400

    rebuilt asteroids

--- games/asteroids/all-classes.js
+++ games/asteroids/all-classes.js
@@ -1017,6 +1017,10 @@ Ext.define('sprites.Bullet', {
             this.force = 0;
         },
 
+        rotateBy: function (incr) {
+            this.rotation += incr;
+        },
+
         update: function () {
             var accel, orientation, ax, ay;
             var max_vel = 5;
@@ -1431,6 +1435,7 @@ Ext.define('interactive.DraggableLayer', {
             });
 
             this.sfx = soundeffects.SoundEffects;
+
         },
 
         onTouch: function (evt, x, y, id, drag) {
@@ -1646,10 +1651,11 @@ Ext.define('interactive.DraggableLayer', {
 
         resize: function () {
 
+            this.gameLayer.resize();
+
             //recenter button
             this.startButton.x = this.gameLayer.canvas_width / 2 - this.startButton.width / 2;
             this.startButton.y = this.gameLayer.canvas_height / 2 - this.startButton.height / 2;
-            this.gameLayer.resize();
 
             //resize touchPad
             this.touchPad.width = this.gameLayer.canvas_width;
@@ -1801,10 +1807,10 @@ Ext.define('interactive.DraggableLayer', {
                 //to make the ship easier to control
                 //these are exclusive -- the axis with the greater magnitude
                 //component of the drag vector is used
-                //set rotational velocity from size of drag
                 if (this.thrustVector) {
 
                     if (Math.abs(this.thrustVector.getOffsetX()) > Math.abs(this.thrustVector.getOffsetY())) {
+
                         //do rotation if offset is above threshold
                         if (Math.abs(this.thrustVector.getOffsetX()) > 10) {
                             if (this.thrustVector.end.x > this.thrustVector.start.x) {
@@ -1813,6 +1819,7 @@ Ext.define('interactive.DraggableLayer', {
                                 this.ship.turnLeft();
                             }
                         }
+
                     } else {
                         //do thrust 
                         force = Math.abs(this.thrustVector.getOffsetY() / 500);
